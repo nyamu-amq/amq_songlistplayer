0a92fcad7d18d9e2dda9bff16705947e4ed78c21
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2020-05-13 08:48:41 +0900

    Add files via upload

--- js/display.js
+++ js/display.js
@@ -371,7 +371,8 @@ function playnextsong() {
 	}
 	else {
 		var index=playlist.findIndex((x)=>x==cursong);
-		nextindex=playlist[index+1];
+		if(index<playlist.length-1) index++; else index=0;
+		nextindex=playlist[index];
 	}
 	playsong(nextindex);
 }
