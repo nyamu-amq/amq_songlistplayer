e40dabdb47654860366e5c86f276d6a15dfc7b4c
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2020-05-14 04:23:18 +0900

    Add files via upload

--- js/display.js
+++ js/display.js
@@ -1,4 +1,5 @@
-let cursong=0;
+var cursong=-1;
+var prevsong;
 
 function loadData() {
     playerNames.clear();
@@ -183,146 +184,156 @@ function updateScoreboardHighlight(name) {
 }
 
 function updateInfo(song) {
+	var cursongdata=song;
+	$("#slInfoHeader").children().remove();
+	if($("#slPlayOrder").val() === "random") {
+		$("#slInfoHeader").append("<h2>Previous Song Info</h2>");
+		if(prevsong!==undefined) song=importData[prevsong];
+		else song=undefined;
+	}
+	else $("#slInfoHeader").append("<h2>Song Info</h2>");
 	stopsong();
     clearInfo();
-    let infoRow1 = $("<div></div>")
-        .attr("class", "slInfoRow");
-    let infoRow2 = $("<div></div>")
-        .attr("class", "slInfoRow");
-    let infoRow3 = $("<div></div>")
-        .attr("class", "slInfoRow");
-    let infoRow4 = $("<div></div>")
-        .attr("class", "slInfoRow");
-    
-    let guesses = song.players.filter((tmpPlayer) => tmpPlayer.correct === true);
+    if(song!==undefined) {
+	    let infoRow1 = $("<div></div>")
+	        .attr("class", "slInfoRow");
+	    let infoRow2 = $("<div></div>")
+	        .attr("class", "slInfoRow");
+	    let infoRow3 = $("<div></div>")
+	        .attr("class", "slInfoRow");
+	    let infoRow4 = $("<div></div>")
+	        .attr("class", "slInfoRow");
+	    
+	    let guesses = song.players.filter((tmpPlayer) => tmpPlayer.correct === true);
 
-    let infoSongName = $("<div></div>")
-        .attr("id", "slInfoSongName")
-        .html("<h5><b>Song Name</b></h5><p>" + song.name + "</p>");
-    let infoArtist = $("<div></div>")
-        .attr("id", "slInfoArtist")
-        .html("<h5><b>Artist</b></h5><p>" + song.artist + "</p>");
-    let infoAnimeEnglish = $("<div></div>")
-        .attr("id", "slInfoAnimeEnglish")
-        .html("<h5><b>Anime English</b></h5><p>" + song.anime.english + "</p>");
-    let infoAnimeRomaji = $("<div></div>")
-        .attr("id", "slInfoAnimeRomaji")
-        .html("<h5><b>Anime Romaji</b></h5><p>" + song.anime.romaji + "</p>");
-    let infoType = $("<div></div>")
-        .attr("id", "slInfoType")
-        .html("<h5><b>Type</b></h5><p>" + song.type + "</p>");
-    let infoSample = $("<div></div>")
-        .attr("id", "slInfoSample")
-        .html("<h5><b>Sample Point</b></h5><p>" + formatSamplePoint(song.startSample, song.videoLength) + "</p>");
-    let infoGuessed = $("<div></div>")
-        .attr("id", "slInfoGuessed")
-        .html("<h5><b>Guessed<br>" + guesses.length + "/" + song.activePlayers + " (" + parseFloat((guesses.length/song.activePlayers*100).toFixed(2)) + "%)</b></h5>");
-    let infoFromList = $("<div></div>")
-        .attr("id", "slInfoFromList")
-        .html("<h5><b>From Lists<br>" + song.fromList.length + "/" + song.totalPlayers + " (" + parseFloat((song.fromList.length/song.totalPlayers*100).toFixed(2)) + "%)</b></h5>");
-    let infoUrls = $("<div></div>")
-        .attr("id", "slInfoUrls")
-        .html("<h5><b>URLs</b></h5>");
+	    let infoSongName = $("<div></div>")
+	        .attr("id", "slInfoSongName")
+	        .html("<h5><b>Song Name</b></h5><p>" + song.name + "</p>");
+	    let infoArtist = $("<div></div>")
+	        .attr("id", "slInfoArtist")
+	        .html("<h5><b>Artist</b></h5><p>" + song.artist + "</p>");
+	    let infoAnimeEnglish = $("<div></div>")
+	        .attr("id", "slInfoAnimeEnglish")
+	        .html("<h5><b>Anime English</b></h5><p>" + song.anime.english + "</p>");
+	    let infoAnimeRomaji = $("<div></div>")
+	        .attr("id", "slInfoAnimeRomaji")
+	        .html("<h5><b>Anime Romaji</b></h5><p>" + song.anime.romaji + "</p>");
+	    let infoType = $("<div></div>")
+	        .attr("id", "slInfoType")
+	        .html("<h5><b>Type</b></h5><p>" + song.type + "</p>");
+	    let infoSample = $("<div></div>")
+	        .attr("id", "slInfoSample")
+	        .html("<h5><b>Sample Point</b></h5><p>" + formatSamplePoint(song.startSample, song.videoLength) + "</p>");
+	    let infoGuessed = $("<div></div>")
+	        .attr("id", "slInfoGuessed")
+	        .html("<h5><b>Guessed<br>" + guesses.length + "/" + song.activePlayers + " (" + parseFloat((guesses.length/song.activePlayers*100).toFixed(2)) + "%)</b></h5>");
+	    let infoFromList = $("<div></div>")
+	        .attr("id", "slInfoFromList")
+	        .html("<h5><b>From Lists<br>" + song.fromList.length + "/" + song.totalPlayers + " (" + parseFloat((song.fromList.length/song.totalPlayers*100).toFixed(2)) + "%)</b></h5>");
+	    let infoUrls = $("<div></div>")
+	        .attr("id", "slInfoUrls")
+	        .html("<h5><b>URLs</b></h5>");
 
-    infoRow1.append(infoSongName);
-    infoRow1.append(infoArtist);
-    infoRow1.append(infoType);
+	    infoRow1.append(infoSongName);
+	    infoRow1.append(infoArtist);
+	    infoRow1.append(infoType);
 
-    infoRow2.append(infoAnimeEnglish);
-    infoRow2.append(infoAnimeRomaji);
-    infoRow2.append(infoSample);
+	    infoRow2.append(infoAnimeEnglish);
+	    infoRow2.append(infoAnimeRomaji);
+	    infoRow2.append(infoSample);
 
-    infoRow3.append(infoUrls);
+	    infoRow3.append(infoUrls);
 
-    infoRow4.append(infoGuessed);
-    infoRow4.append(infoFromList);
+	    infoRow4.append(infoGuessed);
+	    infoRow4.append(infoFromList);
 
-    let infoListContainer;
-    if (song.fromList.length === 0) {
-        infoGuessed.css("width", "98%");
-        infoFromList.hide();
-        if (guesses.length > 1) {
-            let infoGuessedLeft = $("<ul></ul>")
-                .attr("id", "slInfoGuessedLeft");
-            let infoGuessedRight = $("<ul></ul>")
-                .attr("id", "slInfoGuessedRight");
-            let i = 0;
-            for (let guessed of guesses) {
-                if (i++ % 2 === 0) {
-                    infoGuessedLeft.append($("<li></li>")
-                        .text(guessed.name + " (" + guessed.score + ")")
-                    );
-                }
-                else {
-                    infoGuessedRight.append($("<li></li>")
-                        .text(guessed.name + " (" + guessed.score + ")")
-                    );
-                }
-            }
-            infoGuessed.append(infoGuessedLeft);
-            infoGuessed.append(infoGuessedRight);
-        }
-        else {
-            infoListContainer = $("<ul></ul>")
-                .attr("id", "slInfoGuessedList");
-            for (let guessed of guesses) {
-                infoListContainer.append($("<li></li>")
-                    .text(guessed.name + " (" + guessed.score + ")")
-                );
-            }
-            infoGuessed.append(infoListContainer);
-        }
-    }
-    else {
-        infoGuessed.css("width", "");
-        infoListContainer = $("<ul></ul>")
-            .attr("id", "slInfoGuessedList");
-        infoFromList.show();
-        for (let guessed of guesses) {
-            infoListContainer.append($("<li></li>")
-                .text(guessed.name + " (" + guessed.score + ")")
-            );
-        }
-        infoGuessed.append(infoListContainer);
-    }
-    let listStatus = {
-        1: "Watching",
-        2: "Completed",
-        3: "On-Hold",
-        4: "Dropped",
-        5: "Plan to Watch",
-        6: "Looted"
-    };
+	    let infoListContainer;
+	    if (song.fromList.length === 0) {
+	        infoGuessed.css("width", "98%");
+	        infoFromList.hide();
+	        if (guesses.length > 1) {
+	            let infoGuessedLeft = $("<ul></ul>")
+	                .attr("id", "slInfoGuessedLeft");
+	            let infoGuessedRight = $("<ul></ul>")
+	                .attr("id", "slInfoGuessedRight");
+	            let i = 0;
+	            for (let guessed of guesses) {
+	                if (i++ % 2 === 0) {
+	                    infoGuessedLeft.append($("<li></li>")
+	                        .text(guessed.name + " (" + guessed.score + ")")
+	                    );
+	                }
+	                else {
+	                    infoGuessedRight.append($("<li></li>")
+	                        .text(guessed.name + " (" + guessed.score + ")")
+	                    );
+	                }
+	            }
+	            infoGuessed.append(infoGuessedLeft);
+	            infoGuessed.append(infoGuessedRight);
+	        }
+	        else {
+	            infoListContainer = $("<ul></ul>")
+	                .attr("id", "slInfoGuessedList");
+	            for (let guessed of guesses) {
+	                infoListContainer.append($("<li></li>")
+	                    .text(guessed.name + " (" + guessed.score + ")")
+	                );
+	            }
+	            infoGuessed.append(infoListContainer);
+	        }
+	    }
+	    else {
+	        infoGuessed.css("width", "");
+	        infoListContainer = $("<ul></ul>")
+	            .attr("id", "slInfoGuessedList");
+	        infoFromList.show();
+	        for (let guessed of guesses) {
+	            infoListContainer.append($("<li></li>")
+	                .text(guessed.name + " (" + guessed.score + ")")
+	            );
+	        }
+	        infoGuessed.append(infoListContainer);
+	    }
+	    let listStatus = {
+	        1: "Watching",
+	        2: "Completed",
+	        3: "On-Hold",
+	        4: "Dropped",
+	        5: "Plan to Watch",
+	        6: "Looted"
+	    };
 
-    infoListContainer = $("<ul></ul>");
-    for (let fromList of song.fromList) {
-        infoListContainer.append($("<li></li>")
-            .text(fromList.name + " (" + listStatus[fromList.listStatus] + ((fromList.score !== null) ? ", " + fromList.score + ")" : ")"))
-        );
-    }
-    infoFromList.append(infoListContainer);
+	    infoListContainer = $("<ul></ul>");
+	    for (let fromList of song.fromList) {
+	        infoListContainer.append($("<li></li>")
+	            .text(fromList.name + " (" + listStatus[fromList.listStatus] + ((fromList.score !== null) ? ", " + fromList.score + ")" : ")"))
+	        );
+	    }
+	    infoFromList.append(infoListContainer);
 
-    infoListContainer = $("<ul></ul>");
-    for (let host in song.urls) {
-        for (let resolution in song.urls[host]) {
-            let url = song.urls[host][resolution];
-            let innerHTML = "";
-            innerHTML += (host === "catbox" ? "Catbox " : (host === "animethemes" ? "AnimeThemes " : "OpeningsMoe "));
-            innerHTML += (resolution === "0") ? "MP3: " : (resolution === "480") ? "480p: " : "720p: ";
-            innerHTML += "<a href=\"" + url + "\" target=\"_blank\">" + url + "</a>";
-            infoListContainer.append($("<li></li>")
-                .html(innerHTML)
-            );
-        }
-    }
-    infoUrls.append(infoListContainer);
+	    infoListContainer = $("<ul></ul>");
+	    for (let host in song.urls) {
+	        for (let resolution in song.urls[host]) {
+	            let url = song.urls[host][resolution];
+	            let innerHTML = "";
+	            innerHTML += (host === "catbox" ? "Catbox " : (host === "animethemes" ? "AnimeThemes " : "OpeningsMoe "));
+	            innerHTML += (resolution === "0") ? "MP3: " : (resolution === "480") ? "480p: " : "720p: ";
+	            innerHTML += "<a href=\"" + url + "\" target=\"_blank\">" + url + "</a>";
+	            infoListContainer.append($("<li></li>")
+	                .html(innerHTML)
+	            );
+	        }
+	    }
+	    infoUrls.append(infoListContainer);
 
-    $("#slInfoBody").append(infoRow1);
-    $("#slInfoBody").append(infoRow2);
-    $("#slInfoBody").append(infoRow3);
-    $("#slInfoBody").append(infoRow4);
-    
-    cursong=song.songid;
+	    $("#slInfoBody").append(infoRow1);
+	    $("#slInfoBody").append(infoRow2);
+	    $("#slInfoBody").append(infoRow3);
+	    $("#slInfoBody").append(infoRow4);
+	}
+    song=cursongdata;
+    prevsong=cursong=song.songid;
 
 	var length= parseInt($("#slLength").val());
 
